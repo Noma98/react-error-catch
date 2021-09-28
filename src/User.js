cc8f39304904a4100be1d3ae845fa6900a3aa51a
Noma <wiostz98kr@gmail.com>
2021-09-28 09:58:35 +0900

    Error handling using if statement and defaultProps

---
+++ src/User.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function User({ user, onToggle }) {
+    if (!user) return null;
+    return (
+        <div>
+            <div>
+                <b>ID</b>: {user.id}
+            </div>
+            <div onClick={() => onToggle(user.username)}>
+                <b>Username:</b> {user.username}
+            </div>
+        </div>
+    );
+}
+User.defaultProps = {
+    onToggle: () => {
+        console.warn('onToggle is missing!');
+    }
+}
+export default User;
